f9261d1df8cde3850c34772514b68dd68f4ef0fa
LeonHe <hjjleon9527@gmail.com>
2019-01-25 22:20:50 +0800

    添加本地代码

---
+++ Inc/main.h
@@ -0,0 +1,158 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * This notice applies to any and all portions of this file
+  * that are not between comment pairs USER CODE BEGIN and
+  * USER CODE END. Other portions of this file, whether 
+  * inserted by the user or by software development tools
+  * are owned by their respective copyright owners.
+  *
+  * Copyright (c) 2019 STMicroelectronics International N.V. 
+  * All rights reserved.
+  *
+  * Redistribution and use in source and binary forms, with or without 
+  * modification, are permitted, provided that the following conditions are met:
+  *
+  * 1. Redistribution of source code must retain the above copyright notice, 
+  *    this list of conditions and the following disclaimer.
+  * 2. Redistributions in binary form must reproduce the above copyright notice,
+  *    this list of conditions and the following disclaimer in the documentation
+  *    and/or other materials provided with the distribution.
+  * 3. Neither the name of STMicroelectronics nor the names of other 
+  *    contributors to this software may be used to endorse or promote products 
+  *    derived from this software without specific written permission.
+  * 4. This software, including modifications and/or derivative works of this 
+  *    software, must execute solely and exclusively on microcontroller or
+  *    microprocessor devices manufactured by or for STMicroelectronics.
+  * 5. Redistribution and use of this software other than as permitted under 
+  *    this license is void and will automatically terminate your rights under 
+  *    this license. 
+  *
+  * THIS SOFTWARE IS PROVIDED BY STMICROELECTRONICS AND CONTRIBUTORS "AS IS" 
+  * AND ANY EXPRESS, IMPLIED OR STATUTORY WARRANTIES, INCLUDING, BUT NOT 
+  * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A 
+  * PARTICULAR PURPOSE AND NON-INFRINGEMENT OF THIRD PARTY INTELLECTUAL PROPERTY
+  * RIGHTS ARE DISCLAIMED TO THE FULLEST EXTENT PERMITTED BY LAW. IN NO EVENT 
+  * SHALL STMICROELECTRONICS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+  * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+  * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, 
+  * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF 
+  * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING 
+  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+  * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f4xx_hal.h"
+#include "stm32f4xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define PH_STA_Pin GPIO_PIN_13
+#define PH_STA_GPIO_Port GPIOC
+#define PH_MS_Pin GPIO_PIN_14
+#define PH_MS_GPIO_Port GPIOC
+#define LCD_BL_Pin GPIO_PIN_15
+#define LCD_BL_GPIO_Port GPIOC
+#define POWER_SET_Pin GPIO_PIN_0
+#define POWER_SET_GPIO_Port GPIOC
+#define PM_STDBY_Pin GPIO_PIN_1
+#define PM_STDBY_GPIO_Port GPIOC
+#define PM_CHRG_Pin GPIO_PIN_2
+#define PM_CHRG_GPIO_Port GPIOC
+#define KEY_SELECT_Pin GPIO_PIN_3
+#define KEY_SELECT_GPIO_Port GPIOC
+#define KEY_START_Pin GPIO_PIN_1
+#define KEY_START_GPIO_Port GPIOA
+#define DEBUG_TXD_Pin GPIO_PIN_2
+#define DEBUG_TXD_GPIO_Port GPIOA
+#define DEBUG_TXDA3_Pin GPIO_PIN_3
+#define DEBUG_TXDA3_GPIO_Port GPIOA
+#define PH_L_Pin GPIO_PIN_4
+#define PH_L_GPIO_Port GPIOA
+#define PH_R_Pin GPIO_PIN_5
+#define PH_R_GPIO_Port GPIOA
+#define KEY_UP_Pin GPIO_PIN_6
+#define KEY_UP_GPIO_Port GPIOA
+#define KEY_RIGHT_Pin GPIO_PIN_7
+#define KEY_RIGHT_GPIO_Port GPIOA
+#define KEY_LEFT_Pin GPIO_PIN_4
+#define KEY_LEFT_GPIO_Port GPIOC
+#define KEY_DOWN_Pin GPIO_PIN_5
+#define KEY_DOWN_GPIO_Port GPIOC
+#define POWER_VIN_Pin GPIO_PIN_0
+#define POWER_VIN_GPIO_Port GPIOB
+#define PH_MIC_Pin GPIO_PIN_1
+#define PH_MIC_GPIO_Port GPIOB
+#define KEY_B_Pin GPIO_PIN_12
+#define KEY_B_GPIO_Port GPIOB
+#define KEY_Y_Pin GPIO_PIN_13
+#define KEY_Y_GPIO_Port GPIOB
+#define KEY_X_Pin GPIO_PIN_14
+#define KEY_X_GPIO_Port GPIOB
+#define KEY_A_Pin GPIO_PIN_15
+#define KEY_A_GPIO_Port GPIOB
+#define SDIO_STA_Pin GPIO_PIN_15
+#define SDIO_STA_GPIO_Port GPIOA
+#define LCD_CLK_Pin GPIO_PIN_3
+#define LCD_CLK_GPIO_Port GPIOB
+#define LCD_DAT_Pin GPIO_PIN_5
+#define LCD_DAT_GPIO_Port GPIOB
+#define LCD_CS_Pin GPIO_PIN_6
+#define LCD_CS_GPIO_Port GPIOB
+#define LCD_RST_Pin GPIO_PIN_7
+#define LCD_RST_GPIO_Port GPIOB
+#define LCD_DC_Pin GPIO_PIN_8
+#define LCD_DC_GPIO_Port GPIOB
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
---
+++ Src/main.c
@@ -0,0 +1,334 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * This notice applies to any and all portions of this file
+  * that are not between comment pairs USER CODE BEGIN and
+  * USER CODE END. Other portions of this file, whether 
+  * inserted by the user or by software development tools
+  * are owned by their respective copyright owners.
+  *
+  * Copyright (c) 2019 STMicroelectronics International N.V. 
+  * All rights reserved.
+  *
+  * Redistribution and use in source and binary forms, with or without 
+  * modification, are permitted, provided that the following conditions are met:
+  *
+  * 1. Redistribution of source code must retain the above copyright notice, 
+  *    this list of conditions and the following disclaimer.
+  * 2. Redistributions in binary form must reproduce the above copyright notice,
+  *    this list of conditions and the following disclaimer in the documentation
+  *    and/or other materials provided with the distribution.
+  * 3. Neither the name of STMicroelectronics nor the names of other 
+  *    contributors to this software may be used to endorse or promote products 
+  *    derived from this software without specific written permission.
+  * 4. This software, including modifications and/or derivative works of this 
+  *    software, must execute solely and exclusively on microcontroller or
+  *    microprocessor devices manufactured by or for STMicroelectronics.
+  * 5. Redistribution and use of this software other than as permitted under 
+  *    this license is void and will automatically terminate your rights under 
+  *    this license. 
+  *
+  * THIS SOFTWARE IS PROVIDED BY STMICROELECTRONICS AND CONTRIBUTORS "AS IS" 
+  * AND ANY EXPRESS, IMPLIED OR STATUTORY WARRANTIES, INCLUDING, BUT NOT 
+  * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A 
+  * PARTICULAR PURPOSE AND NON-INFRINGEMENT OF THIRD PARTY INTELLECTUAL PROPERTY
+  * RIGHTS ARE DISCLAIMED TO THE FULLEST EXTENT PERMITTED BY LAW. IN NO EVENT 
+  * SHALL STMICROELECTRONICS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+  * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+  * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, 
+  * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF 
+  * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING 
+  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+  * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "adc.h"
+#include "dac.h"
+#include "dma.h"
+#include "sdio.h"
+#include "spi.h"
+#include "tim.h"
+#include "usart.h"
+#include "usb_device.h"
+#include "gpio.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include "oled.h"
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+
+/* USER CODE BEGIN PV */
+//FRESULT f_res;                    /* 文件操作结果 */
+//FATFS fs;													/* FatFs文件系统对象 */
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+/**
+  * 函数功能: FatFS文件系统操作结果信息处理.
+  * 输入参数: FatFS文件系统操作结果：FRESULT
+  * 返 回 值: 无
+  * 说    明: 无
+  */
+/*static void printf_fatfs_error(FRESULT fresult)
+{
+  switch(fresult)
+  {
+    case FR_OK:                   //(0)
+      printf("》操作成功。\n");
+    break;
+    case FR_DISK_ERR:             //(1)
+      printf("！！硬件输入输出驱动出错。\n");
+    break;
+    case FR_INT_ERR:              //(2)
+      printf("！！断言错误。\n");
+    break;
+    case FR_NOT_READY:            //(3)
+      printf("！！物理设备无法工作。\n");
+    break;
+    case FR_NO_FILE:              //(4)
+      printf("！！无法找到文件。\n");
+    break;
+    case FR_NO_PATH:              //(5)
+      printf("！！无法找到路径。\n");
+    break;
+    case FR_INVALID_NAME:         //(6)
+      printf("！！无效的路径名。\n");
+    break;
+    case FR_DENIED:               //(7)
+    case FR_EXIST:                //(8)
+      printf("！！拒绝访问。\n");
+    break;
+    case FR_INVALID_OBJECT:       //(9)
+      printf("！！无效的文件或路径。\n");
+    break;
+    case FR_WRITE_PROTECTED:      //(10)
+      printf("！！逻辑设备写保护。\n");
+    break;
+    case FR_INVALID_DRIVE:        //(11)
+      printf("！！无效的逻辑设备。\n");
+    break;
+    case FR_NOT_ENABLED:          //(12)
+      printf("！！无效的工作区。\n");
+    break;
+    case FR_NO_FILESYSTEM:        //(13)
+      printf("！！无效的文件系统。\n");
+    break;
+    case FR_MKFS_ABORTED:         //(14)
+      printf("！！因函数参数问题导致f_mkfs函数操作失败。\n");
+    break;
+    case FR_TIMEOUT:              //(15)
+      printf("！！操作超时。\n");
+    break;
+    case FR_LOCKED:               //(16)
+      printf("！！文件被保护。\n");
+    break;
+    case FR_NOT_ENOUGH_CORE:      //(17)
+      printf("！！长文件名支持获取堆空间失败。\n");
+    break;
+    case FR_TOO_MANY_OPEN_FILES:  //(18)
+      printf("！！打开太多文件。\n");
+    break;
+    case FR_INVALID_PARAMETER:    // (19)
+      printf("！！参数无效。\n");
+    break;
+  }
+}*/
+uint32_t Buffer_Block_Tx[512*8]; // 写数据缓存
+uint32_t Buffer_Block_Rx[512*8];
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+  /* USER CODE BEGIN 1 */
+  HAL_SD_StateTypeDef sdState;
+  HAL_StatusTypeDef state;
+  HAL_SD_CardInfoTypeDef SDCardInfo;
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_DMA_Init();
+  MX_USB_DEVICE_Init();
+  MX_USART2_UART_Init();
+  MX_DAC_Init();
+  MX_ADC1_Init();
+  MX_SDIO_SD_Init();
+  MX_SPI1_Init();
+  MX_TIM13_Init();
+  /* USER CODE BEGIN 2 */
+
+  HAL_GPIO_WritePin(POWER_SET_GPIO_Port, POWER_SET_Pin, GPIO_PIN_SET);
+  Lcd_Init();			//初始化OLED  
+	
+	LCD_Clear(BLACK);
+  printf("system is ready\r\n");
+
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+    HAL_Delay(500);
+    LCD_Clear(RED);
+    HAL_Delay(500);
+    LCD_Clear(GREEN);
+    HAL_Delay(500);
+    LCD_Clear(BLUE);
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /**Configure the main internal regulator output voltage 
+  */
+  __HAL_RCC_PWR_CLK_ENABLE();
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+  /**Initializes the CPU, AHB and APB busses clocks 
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLM = 8;
+  RCC_OscInitStruct.PLL.PLLN = 336;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = 7;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /**Initializes the CPU, AHB and APB busses clocks 
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  Period elapsed callback in non blocking mode
+  * @note   This function is called  when TIM14 interrupt took place, inside
+  * HAL_TIM_IRQHandler(). It makes a direct call to HAL_IncTick() to increment
+  * a global variable "uwTick" used as application time base.
+  * @param  htim : TIM handle
+  * @retval None
+  */
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
+{
+  /* USER CODE BEGIN Callback 0 */
+
+  /* USER CODE END Callback 0 */
+  if (htim->Instance == TIM14) {
+    HAL_IncTick();
+  }
+  /* USER CODE BEGIN Callback 1 */
+
+  /* USER CODE END Callback 1 */
+}
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{ 
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     tex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
